8093bce7c0ff5a9beabeab49af6a9c85f4856a55
amiralivali <amiralivali123123@gmail.com>
2025-08-31 07:05:10 +0330

    enable migration

---
+++ Visit.DAL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Visit.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<global::Visit.DAL.VisitDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(global::Visit.DAL.VisitDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
